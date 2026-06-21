abc93e0149631e3ff161b45418b266f6513fbcf6
Adam Pelc <sikulin94@gmail.com>
2026-06-21 20:39:14 +0200

    Move code to reserved repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/adagit94/t
+
+go 1.26.4
---
+++ primitive.go
@@ -0,0 +1,12 @@
+package t
+
+type Int interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~uintptr
+}
+
+type Float interface {
+	~float32 | ~float64
+}
+
+type Primitive interface{ Int | Float | ~bool | ~string }
